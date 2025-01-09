bc824719e0a02b6f85867beac2a15f0c166049f0
lucyann <lucyann78@gmail.com>
2025-01-09 11:22:01 -0400

    Header file

---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#define MAX_ARGS 10
+
+char *_getenv(const char *name);
+char **parse_input(char *input);
+int execute_command(char **args);
+int find_or_execute_command(char **args);
+int env_fetch(char **args, char *input, int count);
+extern char **environ;
+
+#endif /* SHELL_H */
+
+
